7c384a4838f52a23d5eb3e3d3cb91daa3467aff9
Gian ALves <gian.quinhones@gmail.com>
2023-07-24 13:53:04 -0300

    Column adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import Image from 'next/image'
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 export default function Home() {
   return (
@@ -7,44 +6,47 @@ export default function Home() {
       <header className="bg-white text-black p-4 flex items-center justify-between">
         <div className="flex items-center">
           <div className="logo">
-            <img
-              src="/logo.png"
-              alt="Logo da Empresa"
-              className="w-10 h-10"
-            />
+            <img src="/logo.png" alt="Logo da Empresa" className="w-10 h-10" />
           </div>
         </div>
 
         <nav className="flex space-x-4">
-          <a href="/" className="hover:bg-gray-300 py-1 px-2 rounded">Overview</a>
-          <a href="/sobre" className="hover:bg-gray-300 py-1 px-2 rounded">Projects</a>
-          <a href="/contato" className="hover:bg-gray-300 py-1 px-2 rounded">Contact</a>
+          <a href="/" className="hover:bg-gray-300 py-1 px-2 rounded">
+            Overview
+          </a>
+          <a href="/sobre" className="hover:bg-gray-300 py-1 px-2 rounded">
+            Projects
+          </a>
+          <a href="/contato" className="hover:bg-gray-300 py-1 px-2 rounded">
+            Contact
+          </a>
         </nav>
 
         <div>
           <i className="fas fa-search text-2xl cursor-pointer hover:text-gray-300"></i>
         </div>
       </header>
-      <div className='content-info text-black'>
-        <div className='grid grid-cols-2 gap-4'>
-          <section className='user-info'>
+      <div className="content-info text-black">
+        <div className="grid grid-cols-12 gap-4">
+          <section className="col-span-8 user-info">
             <div className="profile-cover">
               <div className="cover-gradient"></div>
             </div>
 
             <div className="profile-info">
-              <img src="/photo.jpg" alt="Imagem de Perfil" className="profile-picture" />
-              <div className="user-info">
-                Gian Lucas Quinhones Alves
-              </div>
+              <img
+                src="/photo.jpg"
+                alt="Imagem de Perfil"
+                className="profile-picture"
+              />
+              <div className="user-info">Gian Lucas Quinhones Alves</div>
             </div>
-
           </section>
-          <section>
+          <section className="col-span-4 contact-info">
             teste 2
           </section>
         </div>
       </div>
     </main>
-  )
+  );
 }
