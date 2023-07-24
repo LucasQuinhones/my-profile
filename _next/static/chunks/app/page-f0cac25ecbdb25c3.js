cf8858b26bd014ade530e42c570c5ab83709bb80
Gian ALves <gian.quinhones@gmail.com>
2023-07-24 16:28:07 -0300

    Updates

---
+++ _next/static/chunks/app/page-f0cac25ecbdb25c3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{1495:function(n,e,u){Promise.resolve().then(u.t.bind(u,352,23))},352:function(){}},function(n){n.O(0,[972,971,596,744],function(){return n(n.s=1495)}),_N_E=n.O()}]);
